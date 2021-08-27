be17b11d0615e9249d5e501bb83a3c170108ab52
thewebmechanic <80426417+thewebmechanic@users.noreply.github.com>
2021-08-27 16:13:49 +0300

    added js for particles effect

--- script.js
+++ script.js
@@ -11,5 +11,52 @@ myImage.addEventListener('load', function(){
     canvas.height = 1689;
     
     ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
+
+    let particlesArray = [];
+    const numberOfParticles = 5000;
+
+    class Particle {
+        constructor (){
+            this.x = Math.random() * canvas.width;
+            this.y = 0;
+            this.speed = 0;
+            this.velocity = Math.random() * 0.5;
+            this.size = Math.random() * 1.5 + 1;
+
+        }
+
+        update() {
+            this.y+= this.velocity;
+            if (this.y >= canvas.height) {
+                this.y = 0;
+                this.x = Math.random() * canvas.width;
+            }
+        }
+
+        draw(){
+            ctx.beginPath();
+            ctx.fillStyle = 'white';
+            ctx.arc(this.x, this.y, this.size, 0, Math.PI *2 );
+            ctx.fill();
+        }
+    }
+    function init() {
+
+        for(let i = 0; i<numberOfParticles; i++) {
+            particlesArray.push(new Particle);
+        }
+    }
+    init();
+    function animate() {
+        ctx.globalAlpha = 0.05;
+        ctx.fillStyle = 'rgb(0,0,0)';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        for (let i = 0; i < particlesArray.length; i++) {
+            particlesArray[i].update();
+            particlesArray[i].draw();
+        }
+        requestAnimationFrame(animate);
+    }
+    animate();
 });
 
