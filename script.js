3c2d6cbeaea2770051a2a4a3ae9163dc829cf06a
thewebmechanic <80426417+thewebmechanic@users.noreply.github.com>
2021-08-27 16:14:54 +0300

    some changes to js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ myImage.addEventListener('load', function(){
     canvas.width = 1600;
     canvas.height = 1689;
     
-    ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
+    
 
     let particlesArray = [];
     const numberOfParticles = 5000;
@@ -48,6 +48,7 @@ myImage.addEventListener('load', function(){
     }
     init();
     function animate() {
+        ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
         ctx.globalAlpha = 0.05;
         ctx.fillStyle = 'rgb(0,0,0)';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
