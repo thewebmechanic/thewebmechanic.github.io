3c7ff0b55d1c78a4d133a4dfe015be26efae9d2f
thewebmechanic <alexthewebnechanic.com>
2021-09-01 23:53:28 +0300

    added index php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+	<head>
+		<title>Thewebmechanic - Portfolio</title>
+		<meta charset="utf-8" />
+		<meta name="robots" content="index, follow" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="description" content="My portfolio, containing all the projects I've worked on, entirely or partially." />
+		<meta property="og:title" content="Thewebmechanic - Portfolio" />
+        <meta property="og:site_name" content="Thewebmechanic" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:url" content="https://thewebmechanic.github.io/useful-links/" />
+        <meta property="og:image" content="og-image.jpg" /> <!--@TODO-->
+        <meta property="og:image:widht" content="1200" />
+        <meta property="og:image:height" content="630" />
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+	<body>
+	</body>
+</html> 
+
+<?php
+
+phpinfo();
+
+?>
